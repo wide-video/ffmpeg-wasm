48660c6dde77bf796802f93f689ab6d26f767286
Jozef Chutka <jozefchutka@gmail.com>
2022-09-22 08:36:48 +0000

    updated build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,6 +21,7 @@ $SCRIPT_ROOT/build-rubberband.sh
 $SCRIPT_ROOT/build-theora.sh
 $SCRIPT_ROOT/build-vorbis.sh
 $SCRIPT_ROOT/build-openh264.sh
+$SCRIPT_ROOT/build-kvazaar.sh
 
 $SCRIPT_ROOT/configure-ffmpeg.sh
 $SCRIPT_ROOT/build-ffmpeg.sh
