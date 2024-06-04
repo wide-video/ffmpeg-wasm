7c28c73a8eaf912d7e772c9bbb4b5f020646eb86
Jozef Chutka <jozefchutka@gmail.com>
2024-06-04 07:21:16 +0200

    cleanup

--- scripts/init-emscripten.sh
+++ scripts/init-emscripten.sh
@@ -2,11 +2,7 @@
 
 # run this as `source ./scripts/init-emscripten.sh`
 
-# figure out the latest https://github.com/emscripten-core/emsdk/issues/547#issuecomment-1359134381
-# 3.1.47 OK
-# 3.1.48 - 3.1.56 N/A for Mac M2 https://github.com/emscripten-core/emscripten/issues/22008#issuecomment-2138722249
-# 3.1.57 BROKEN https://github.com/emscripten-core/emscripten/issues/22008
-# 3.1.58 BROKEN https://github.com/emscripten-core/emscripten/issues/21989
+# 3.1.47 https://github.com/emscripten-core/emscripten/issues/22008#issuecomment-2141336106
 EMSDK_VERSION="3.1.47"
 
 MODULE_ROOT=$(dirname $(dirname $0))/modules/emsdk
